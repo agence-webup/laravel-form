45c89d5106d890f2f1a6cd6010c21945ed36e250
Baptiste DUCATEL <baptiste@agence-webup.com>
2016-04-13 16:18:51 +0200

    fix radio required star

--- src/views/radio.blade.php
+++ src/views/radio.blade.php
@@ -1,5 +1,5 @@
 <div class="{{$config['divClass']}} {{$wrapperClass}} @if($errors){{$config['errorClass']}} @endif">
-    <label>{!!$label!!}</label>
+    <label>{!!$label!!} <i class="{{$config['requiredClass']}}">*</i></label>
     @if($errors)
     <ul class="{{$config['errorMessageClass']}}">
         @foreach ($errors as $error)
@@ -13,14 +13,14 @@
         type="{{$type}}"
         id="{{$radio['id']}}"
         name="{{$name}}"
-        @if($value == $radio['value']) checked @endif
+        @if($value !== null && $value == $radio['value']) checked @endif
         value="{{$radio['value']}}"
         @if($required)required @endif
         @foreach($radio['attr'] as $key => $val)
         @if(is_int($key)){{$val}}@else{{$key}}="{{$val}}"@endif
         @endforeach
         >
-        {{$radio['label']}} @if($required)<i class="{{$config['requiredClass']}}">*</i>@endif
+        {{$radio['label']}} @if($required)@endif
     </label>
     @endforeach
 </div>
